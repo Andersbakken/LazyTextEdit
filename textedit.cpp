244c6c47f0560627408ced970ac9c7e02f887968
Anders Bakken <anders.bakken@nokia.com>
2009-09-11 20:56:29 -0700

    compile

--- textedit.cpp
+++ textedit.cpp
@@ -1261,9 +1261,9 @@ QRect TextEditPrivate::cursorRect(const TextCursor &cursor) const
 int TextEditPrivate::findLastPageSize() const
 {
     TextEditPrivate p(textEdit);
-    const int startPos = qMax(0, d->document->documentSize() - ((layoutEnd - viewportPosition) * 2)); // ### estimate
+    const int startPos = qMax(0, document->documentSize() - ((layoutEnd - viewportPosition) * 2)); // ### estimate
     p.viewportPosition = startPos;
-    p.relayoutByPosition(d->document->documentSize() - startPos);
+    p.relayoutByPosition(document->documentSize() - startPos);
 
     // ### not done
 
