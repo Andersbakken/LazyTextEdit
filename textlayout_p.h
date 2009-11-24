c898131c91d51e2cf52965d905e48dfc23687dd8
Anders Bakken <anders.bakken@nokia.com>
2009-11-24 15:55:14 +0100

    split up textlayout into two classes (hopefully cleaner)

--- textlayout_p.h
+++ textlayout_p.h
@@ -34,17 +34,47 @@
 QDebug &operator<<(QDebug &str, const QTextLine &line);
 #endif
 
+class TextDocumentBuffer
+{
+public:
+    TextDocumentBuffer(TextDocument *doc) : document(doc), bufferPosition(0) {}
+    virtual ~TextDocumentBuffer() {}
+
+    inline QString bufferRead(int from, int size) const
+    {
+        Q_ASSERT(document);
+        if (from < bufferPosition || from + size > bufferPosition + buffer.size()) {
+            return document->read(from, size);
+        }
+        return buffer.mid(from - bufferPosition, size);
+    }
+    inline QChar bufferReadCharacter(int index) const // document coordinates
+    {
+        Q_ASSERT(document);
+        if (index >= bufferPosition && index < bufferPosition + buffer.size()) {
+            return buffer.at(index - bufferPosition);
+        } else {
+            Q_ASSERT(index >= 0 && index < document->documentSize()); // what if index == documentSize?
+            return document->readCharacter(index);
+        }
+    }
+
+    TextDocument *document;
+    int bufferPosition;
+    QString buffer;
+};
+
 class TextEdit;
-class TextLayout
+class TextLayout : public TextDocumentBuffer
 {
 public:
     enum { MinimumBufferSize = 5000, LeftMargin = 3 };
     TextLayout(TextDocument *doc = 0)
-        : document(doc), textEdit(0), bufferPosition(0),
-          viewportPosition(0), layoutEnd(-1), viewport(-1),
-          visibleLines(-1), lastVisibleCharacter(-1), lastBottomMargin(0),
-          widest(-1), maxViewportPosition(0), layoutDirty(true), sectionsDirty(true),
-          lineBreaking(true), suppressTextEditUpdates(false)
+        : TextDocumentBuffer(doc), textEdit(0),
+        viewportPosition(0), layoutEnd(-1), viewport(-1),
+        visibleLines(-1), lastVisibleCharacter(-1), lastBottomMargin(0),
+        widest(-1), maxViewportPosition(0), layoutDirty(true), sectionsDirty(true),
+        lineBreaking(true), suppressTextEditUpdates(false)
     {
     }
 
@@ -54,10 +84,9 @@ public:
         qDeleteAll(unusedTextLayouts);
     }
 
-    TextDocument *document;
     TextEdit *textEdit;
     QWeakPointer<SyntaxHighlighter> syntaxHighlighter;
-    int bufferPosition, viewportPosition, layoutEnd, viewport, visibleLines,
+    int viewportPosition, layoutEnd, viewport, visibleLines,
         lastVisibleCharacter, lastBottomMargin, widest, maxViewportPosition;
     bool layoutDirty, sectionsDirty, lineBreaking, suppressTextEditUpdates;
     QList<QTextLayout*> textLayouts, unusedTextLayouts;
@@ -65,7 +94,6 @@ public:
     QList<TextEdit::ExtraSelection> extraSelections;
     QList<QPair<int, QTextLine> > lines; // int is start position of line in document coordinates
     QRect contentRect; // contentRect means the laid out area, not just the area currently visible
-    QString buffer;
     QList<TextSection*> sections; // these are all the sections in the buffer. Some might be before the current viewport
     QFont font;
 
@@ -84,6 +112,8 @@ public:
     QTextLayout *layoutForPosition(int pos, int *offset = 0, int *index = 0) const;
 
     int textPositionAt(const QPoint &pos) const;
+    inline int bufferOffset() const { return viewportPosition - bufferPosition; }
+
     QString dump() const;
 
     enum Direction {
@@ -91,24 +121,6 @@ public:
         Backward = TextDocument::FindBackward
     };
     void updateViewportPosition(int pos, Direction direction);
-
-    inline int bufferOffset() const { return viewportPosition - bufferPosition; }
-    QString bufferRead(int from, int size) const
-    {
-        if (from < bufferPosition || from + size > bufferPosition + buffer.size()) {
-            return document->read(from, size);
-        }
-        return buffer.mid(from - bufferPosition, size);
-    }
-    inline QChar bufferReadCharacter(int index) const // document coordinates
-    {
-        if (index >= bufferPosition && index < bufferPosition + buffer.size()) {
-            return buffer.at(index - bufferPosition);
-        } else {
-            Q_ASSERT(index >= 0 && index < document->documentSize()); // what if index == documentSize?
-            return document->readCharacter(index);
-        }
-    }
 };
 
 #endif
