da8116dbe24c7d059142e89e30a81c5d2e666ee7
Anders Bakken <anders.bakken@nokia.com>
2010-01-09 12:09:34 -0800

    It's 2010

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // abcde fghij klmno pqrst uvwxy z1234 56789 0!@#$ abcde fghij klmno pqrst uvwxy z1234 56789 0!@#$
-// Copyright 2009 Anders Bakken
+// Copyright 2010 Anders Bakken
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- textsection_p.h
+++ textsection_p.h
@@ -1,4 +1,4 @@
-// Copyright 2009 Anders Bakken
+// Copyright 2010 Anders Bakken
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
