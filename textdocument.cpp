a08d1e945ce544a688de874be6275853dacffb29
Anders Bakken <anders.bakken@nokia.com>
2010-02-09 08:42:36 -0800

    Remove this warning.

--- textdocument.cpp
+++ textdocument.cpp
@@ -80,9 +80,12 @@ bool TextDocument::load(QIODevice *device, DeviceMode mode, QTextCodec *codec)
     d->documentSize = device->size();
     if (d->documentSize <= d->chunkSize && mode == Sparse && !(options & NoImplicitLoadAll))
         mode = LoadAll;
+#if 0
     if (codec && mode == Sparse) {
+
         qWarning("Sparse mode doesn't really work with unicode data yet. I am working on it.\n--\nAnders");
     }
+#endif
     d->first = d->last = 0;
 
     if (d->device) {
